e770f8249c74dd1aaab66de080b18ed2bb2da75d
AlexanderGarifullin <139747118+AlexanderGarifullin@users.noreply.github.com>
2023-07-17 18:57:37 +0500

    Creating heavy winapi processes (Child)

---
+++ creating/child.c
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	cout << "== == CHILD PROCESS START == ==\n";
+	cout << "I'm a child process" << endl;
+	cout << "== == CHILD PROCESS START == ==\n";
+	exit(0);
+}
